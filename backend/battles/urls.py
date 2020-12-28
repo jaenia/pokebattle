81e5097279510fafabc5b38e59f6c5f115c36059
Jaênia Sousa <jaeniaps@gmail.com>
2020-12-28 17:18:28 -0300

    Put battle urls as default

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'battles'
 urlpatterns = [
+    path('', views.BattleList.as_view(), name='battle_list'),
     path('add/', views.BattleCreate.as_view(), name='battle_create'),
     path('<int:pk>', views.BattleDetail.as_view(), name='battle_detail'),
 ]
