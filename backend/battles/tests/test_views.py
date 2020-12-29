b27aac267e23785b43421b4aed1886df646d0ced
Jaênia Sousa <jaeniaps@gmail.com>
2020-12-29 15:09:29 -0300

    Add tests for views

---
+++ backend/battles/tests/test_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from model_mommy import mommy
+
+
+class BattleListViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_battle_list(self):
+        battle1 = mommy.make('battles.Battle')
+        battle2 = mommy.make('battles.Battle')
+
+        url = reverse('battles:battle_list')
+        response = self.client.get(url)
+
+        self.assertContains(response, battle1.id, status_code=200)
+        self.assertContains(response, battle2.id, status_code=200)
+
+
+class BattleDetailViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_battle_detail(self):
+        battle = mommy.make('battles.Battle')
+
+        url = reverse('battles:battle_detail', args=[battle.id])
+        response = self.client.get(url)
+
+        self.assertContains(response, battle.id, status_code=200)
+
+
+class BattleCreateViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_battle_create(self):
+        data = {
+            'creator': 'creator@test.com',
+            'opponent': 'opponent@test.com'
+        }
+        url = reverse('battles:battle_create')
+        response = self.client.post(url, data, follow=True)
+
+        self.assertEqual(response, 200)
