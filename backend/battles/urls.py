53b922855046419fa8458c53b18b1414d5b2e9e6
Jaênia Sousa <jaeniaps@gmail.com>
2020-12-29 16:41:06 -0300

    Refactor urls

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'battles'
 urlpatterns = [
     path('', views.BattleList.as_view(), name='battle_list'),
-    path('add/', views.BattleCreate.as_view(), name='battle_create'),
+    path('add', views.BattleCreate.as_view(), name='battle_create'),
     path('<int:pk>', views.BattleDetail.as_view(), name='battle_detail'),
 ]
