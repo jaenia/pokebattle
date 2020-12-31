409f9865494c6ee8ec793f7a85d7b6c586447722
Jaênia Sousa <jaeniaps@gmail.com>
2020-12-31 15:36:11 -0300

    Renames related_name of creator and opponent in Battle model

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -5,8 +5,8 @@ from users.models import User
 
 
 class Battle(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='battle_creator', )
-    opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='battle_opponent')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='battles_as_creator', )
+    opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name='battle_as_opponent')
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def get_absolute_url(self):
