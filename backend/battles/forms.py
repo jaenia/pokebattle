05a1800060bfa2f68de600e82a65fd326829d96b
Jaênia Sousa <jaeniaps@gmail.com>
2021-01-04 15:11:37 -0300

    Makes creator field opitional in form

--- backend/battles/forms.py
+++ backend/battles/forms.py
@@ -6,23 +6,20 @@ from users.models import User
 
 
 class BattleForm(forms.ModelForm):
+    creator = forms.ModelChoiceField(required=False, queryset=User.objects.all())
+
     class Meta:
         model = Battle
         fields = ['creator', 'opponent']
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user')
+        self.current_user = kwargs.pop('user')
         super(BattleForm, self).__init__(*args, **kwargs)
-        self.fields['opponent'].queryset = User.objects.exclude(email=user.email)
+        self.fields['opponent'].queryset = User.objects.exclude(email=self.current_user.email)
+        self.fields['creator'].initial = self.current_user
 
     def clean(self):
         cleaned_data = super().clean()
-        creator = cleaned_data.get('creator')
-        opponent = cleaned_data.get('opponent')
-
-        self.cleaned_data['creator'] = User.objects.first()
-
-        if creator == opponent:
-            raise ValidationError('You can not be your own opponent. Please, choose someone else.')
+        cleaned_data['creator'] = self.fields['creator'].initial
 
         return cleaned_data
