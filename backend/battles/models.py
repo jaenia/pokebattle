b5f7c918291137a6f873f68d8405c31e7a3ee455
Jaênia Sousa <jaeniaps@gmail.com>
2021-01-11 17:40:13 -0300

    Adds migrations to fill new fields in existing Battle data

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -17,37 +17,37 @@ class Battle(models.Model):
     creator_pokemon_1 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_creator_pokemon_1"
+        related_name="battle_as_creator_pokemon_1",
     )
     creator_pokemon_2 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_creator_pokemon_2"
+        related_name="battle_as_creator_pokemon_2",
     )
     creator_pokemon_3 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_creator_pokemon_3"
+        related_name="battle_as_creator_pokemon_3",
     )
 
     opponent_pokemon_1 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_opponent_pokemon_1"
+        related_name="battle_as_opponent_pokemon_1",
     )
-    opponent_pokemon_1 = models.ForeignKey(
+    opponent_pokemon_2 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_opponent_pokemon_1"
+        related_name="battle_as_opponent_pokemon_2",
     )
-    opponent_pokemon_1 = models.ForeignKey(
+    opponent_pokemon_3 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
-        related_name="battle_as_opponent_pokemon_1"
+        related_name="battle_as_opponent_pokemon_3",
     )
 
     def __str__(self):
-        return f"Battle #{self.battle.id}: {self.creator.email} X {self.opponent.email}"
+        return f"Battle #{self.id}: {self.creator.email} X {self.opponent.email}"
 
     def get_absolute_url(self):
         return reverse("battles:battle_detail", kwargs={"pk": self.pk})
