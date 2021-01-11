9e8261e83863b5e4fa7a331475a721e79ac0cf11
Jaênia Sousa <jaeniaps@gmail.com>
2021-01-11 11:57:47 -0300

    Fix prospector error related to line too longs

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -23,10 +23,26 @@ class Battle(models.Model):
 
 class BattleTeam(models.Model):
     battle = models.ForeignKey(Battle, on_delete=models.CASCADE, related_name="battleteam_battle")
-    trainer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battleteam_as_trainer")
-    pokemon_1 = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="battleteam_as_pokemon_1")
-    pokemon_2 = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="battleteam_as_pokemon_2")
-    pokemon_3 = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="battleteam_as_pokemon_3")
+    trainer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="battleteam_as_trainer"
+    )
+    pokemon_1 = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name="battleteam_as_pokemon_1"
+    )
+    pokemon_2 = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name="battleteam_as_pokemon_2"
+    )
+    pokemon_3 = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name="battleteam_as_pokemon_3"
+    )
 
     def __str__(self):
         return f"Battle #{self.battle.id}: team of {self.trainer.email}"
