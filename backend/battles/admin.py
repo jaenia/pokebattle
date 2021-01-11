904fb985136140ffa1bb969207085c79a0d1dc1e
Jaênia Sousa <jaeniaps@gmail.com>
2021-01-11 12:01:23 -0300

    Removes comment of admin import from django.contrib in pokemons admin file

--- backend/battles/admin.py
+++ backend/battles/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from battles.models import Battle, BattleTeam
 
-admin.site.register(Battle, BattleTeam)
+admin.site.register(Battle)
+admin.site.register(BattleTeam)
--- backend/pokemons/admin.py
+++ backend/pokemons/admin.py
@@ -1,6 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
 from pokemons.models import Pokemon
 
 admin.site.register(Pokemon)
