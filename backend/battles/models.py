322c7d6a85e96630096af0fe9895111c7fcdb5f4
Jaênia Sousa <jaeniaps@gmail.com>
2021-01-11 16:14:04 -0300

    Deletes BattleTeam model

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -13,7 +13,7 @@ class Battle(models.Model):
     )
     opponent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="battle_as_opponent")
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-    
+
     creator_pokemon_1 = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
@@ -51,30 +51,3 @@ class Battle(models.Model):
 
     def get_absolute_url(self):
         return reverse("battles:battle_detail", kwargs={"pk": self.pk})
-
-
-class BattleTeam(models.Model):
-    battle = models.ForeignKey(Battle, on_delete=models.CASCADE, related_name="battleteam_battle")
-    trainer = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="battleteam_as_trainer"
-    )
-    pokemon_1 = models.ForeignKey(
-        Pokemon,
-        on_delete=models.CASCADE,
-        related_name="battleteam_as_pokemon_1"
-    )
-    pokemon_2 = models.ForeignKey(
-        Pokemon,
-        on_delete=models.CASCADE,
-        related_name="battleteam_as_pokemon_2"
-    )
-    pokemon_3 = models.ForeignKey(
-        Pokemon,
-        on_delete=models.CASCADE,
-        related_name="battleteam_as_pokemon_3"
-    )
-
-    def __str__(self):
-        return f"Battle #{self.battle.id}: team of {self.trainer.email}"
