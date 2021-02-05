280b2d5c3ab2ef469a6d52fe297d8ec5e0858622
Jaênia Sousa <jaeniaps@gmail.com>
2021-02-05 17:16:30 -0300

    Moves email setup to local base file

--- backend/pokebattle/settings/local.py
+++ backend/pokebattle/settings/local.py
@@ -1,9 +1,1 @@
 from .local_base import *  # noqa
-from decouple import config  # noqa
-
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_HOST_USER = config("SENDGRID_USERNAME")
-EMAIL_HOST_PASSWORD = config("SENDGRID_PASSWORD")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
