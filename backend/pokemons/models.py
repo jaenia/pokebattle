7c1766254d628ffb192934e5b46e46533b1b79cf
Jaênia Sousa <jaeniaps@gmail.com>
2021-02-22 13:46:47 -0300

    Adds image_url field to Pokemon model

--- backend/pokemons/models.py
+++ backend/pokemons/models.py
@@ -7,6 +7,7 @@ class Pokemon(models.Model):
     attack = models.IntegerField()
     defense = models.IntegerField()
     hit_points = models.IntegerField()
+    image_url = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
