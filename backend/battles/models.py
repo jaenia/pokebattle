c62d06b9449ed12c1ff7664d288461ff182c15f9
Jaênia Sousa <jaeniaps@gmail.com>
2021-02-25 16:11:35 -0300

    Checks if creator has pokemons in has_been_resolved method

--- backend/battles/models.py
+++ backend/battles/models.py
@@ -82,11 +82,19 @@ class Battle(models.Model):
         return get_battle_result(self)
 
     def has_been_resolved(self):
-        return (
+        creator_has_pokemons = (
+            self.creator_pokemon_1 is not None
+            and self.creator_pokemon_2 is not None
+            and self.creator_pokemon_3 is not None
+        )
+
+        opponent_has_pokemons = (
             self.opponent_pokemon_1 is not None
             and self.opponent_pokemon_2 is not None
             and self.opponent_pokemon_3 is not None
         )
+
+        return creator_has_pokemons and opponent_has_pokemons
 
     def __str__(self):
         return f"Battle #{self.id}: {self.creator.email} X {self.opponent.email}"
