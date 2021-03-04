e5e1ac41a65b489aa8097b0fdc7659f36b6ff267
Jaênia Sousa <jaeniaps@gmail.com>
2021-03-04 11:37:45 -0300

    Adds save pokemons task to celery beat

--- backend/pokebattle/celerybeat_schedule.py
+++ backend/pokebattle/celerybeat_schedule.py
@@ -4,4 +4,8 @@ from celery.schedules import crontab
 CELERYBEAT_SCHEDULE = {
     # Internal tasks
     "clearsessions": {"schedule": crontab(hour=3, minute=0), "task": "users.tasks.clearsessions"},
+    "save_all_pokemons_from_pokeapi": {
+        "schedule": crontab(hour=7, minute=30, day_of_week=5),
+        "task": "pokemon.tasks.save_pokemon_from_pokeapi_weekly",
+    },
 }
