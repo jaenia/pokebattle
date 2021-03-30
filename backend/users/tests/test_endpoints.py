426cf6ce0c7c3b72d36ad9603576de0fe84c809d
Jaênia Sousa <jaeniaps@gmail.com>
2021-03-30 11:15:38 -0300

    Renames test class for create token view

--- backend/users/tests/test_endpoints.py
+++ backend/users/tests/test_endpoints.py
@@ -9,7 +9,7 @@ def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class UserSerializerTests(TestCase):
+class CreateTokenViewTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
