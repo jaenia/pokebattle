3825cdf885b3f1f9afbf3447da8480ffcc119062
Jaênia Sousa <jaeniaps@gmail.com>
2021-04-05 16:07:43 -0300

    Renames users api url
    Replaces user by users

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
     path("oauth/", include("social_django.urls", namespace="social")),
-    path("api/user/", include(("api.users.urls", "users"), namespace="api_users")),
+    path("api/users/", include(("api.users.urls", "users"), namespace="api_users")),
+    path("api/battles", include(("api.battles.urls", "battles"), namespace="api_battles")),
 ]
