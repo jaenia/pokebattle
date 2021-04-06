5fc5e8d06a49036d8bf1bad41bdf3c289430e720
Jaênia Sousa <jaeniaps@gmail.com>
2021-04-06 11:26:02 -0300

    Removes necessity of send the token authentication in the request

--- backend/api/battles/views.py
+++ backend/api/battles/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect  # noqa
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from api.battles.serializers import BattleSerializer
@@ -10,5 +9,4 @@ from battles.models import Battle
 class BattleListCreateEndpoint(generics.ListCreateAPIView):
     serializer_class = BattleSerializer
     queryset = Battle.objects.all()
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
