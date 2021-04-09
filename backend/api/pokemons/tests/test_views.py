882299af1f5234f0062b59c309f86b0ef0934ddd
Jaênia Sousa <jaeniaps@gmail.com>
2021-04-09 16:28:12 -0300

    Removes unused model mommy import from Pokemon api test views

--- backend/api/pokemons/tests/test_views.py
+++ backend/api/pokemons/tests/test_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from model_mommy import mommy
 from rest_framework import status
 from rest_framework.test import APIClient
 
