b2c8d2b4522cf2cfb6fbce2be257b578d4e42c22
Jaênia Sousa <jaeniaps@gmail.com>
2021-04-13 14:50:30 -0300

    Tests if created pokemons are in response content

--- backend/api/pokemons/tests/test_views.py
+++ backend/api/pokemons/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from model_mommy import mommy
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -22,8 +23,13 @@ class PrivatePokemonListEndpointTests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
-    def test_see_users_list(self):
+    def test_see_pokemons_list(self):
+        mommy.make("pokemons.Pokemon", name="pokemon1")
+        mommy.make("pokemons.Pokemon", name="pokemon2")
+
         url = reverse("api_pokemons:pokemon_list")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(b"pokemon1", response.content)
+        self.assertIn(b"pokemon2", response.content)
